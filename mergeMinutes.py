f07e2a8fad66dcf8d67208f5cc928ec0c6bf629e
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2021-03-31 18:29:55 +0300

    aktualizacja 31.03

--- mergeMinutes.py
+++ mergeMinutes.py
@@ -2,7 +2,6 @@ import json
 from datetime import datetime
 from os import listdir
 """
-
 def avg (list):
 	list_sum = 0
 	for i in list:
@@ -10,14 +9,32 @@ def avg (list):
 	avg = list_sum / len(list)
 	return avg
 
-files = listdir(/home/pi/cyano-automaton.github.io/data)
+def byMinute(e):
+  return e['minute']
+
+files = listdir("./data")
 files.remove("right_now.json")
+print (files)
 
 objects=[]
 
 for i in files:
-    with open ("/home/pi/cyano-automaton.github.io/data/"+files[i], "r") as infile:
-        object[i]=json.load(infile)
+	with open ("./data/"+i, "r") as f:
+		x=json.loads(f.read())
+		objects.append(x)
+
+objects.sort(key=byMinute)
+
+
+for i in objects:
+	counter = 0
+	objects[counter]["temp"] = [objects[counter]["temp"]]
+
+	objects[0]["temp"].append(objects[1]["temp"])
+	print(objects[0]["temp"])
+
+for i in objects:
+
 
 object[0].temp = [object[0].temp]
 object[0].tds = [object[0].temp]
@@ -26,5 +43,4 @@ object[0].ntu = [object[0].temp]
 
 for i in objects:
         object[0].temp = [object[0].temp, object[1].temp]
-
 """
