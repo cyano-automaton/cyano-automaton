35d100908095014b0bbef3f6fa6603c1ae8a98c1
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2021-04-09 09:48:12 +0300

    dodatkowa funkcja js

---
+++ last6Hours.js
@@ -0,0 +1,25 @@
+let right_now;
+let font_size = 20;
+
+function preload() {
+  let adres = "./hours/right_now.json";
+  right_now = loadJSON(adres);
+}
+
+function setup() {
+  createCanvas(400, 400);
+  textFont("Roboto Mono");
+  noLoop();
+}
+
+function draw() {
+  background(255, 0);
+  textSize(font_size);
+  text("Right now:",0, font_size)
+  text("Temperature: "+right_now.temp+"°C",0, font_size*4)
+  text("TDS: "+right_now.tds+" ppm",0, font_size*6)
+  text("pH: "+right_now.ph,0, font_size*8)
+  text("Turbidity: "+right_now.ntu+" ntu",0, font_size*10)
+  textAlign(RIGHT);
+  text(right_now.hour+":"+right_now.minute+" "+right_now.day+"."+right_now.month+"." +right_now.year,width, font_size)
+}
