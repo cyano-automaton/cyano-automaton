f4ed5330896f6ec8e83b2bc4abd49ad973880faf
Bartek <bartek@supereclectic.team>
2021-04-14 14:39:28 +0300

    Wed 14 Apr 2021 02:39:28 PM EEST

--- mergeDays.py
+++ mergeDays.py
@@ -14,14 +14,14 @@ def byHour(e):
 def byDay(e):
     return e[0]['day']
 
-files = listdir("./hours/")
+files = listdir("/home/pi/cyano-automaton.github.io/hours/")
 
 print(files)
 
 objects=[]
 
 for i in files:
-	with open ("./hours/"+i, "r") as f:
+	with open ("/home/pi/cyano-automaton.github.io/hours/"+i, "r") as f:
 		x=json.loads(f.read())
 		objects.append(x)
 
@@ -32,14 +32,14 @@ day = []
 
 for i in range(24):
 	day.append(objects[i])
-location = "./days/"
+location = "/home/pi/cyano-automaton.github.io/days/"
 filename =str(objects[i][0]["year"])+"_"+str(objects[i][0]["month"])+"_"+str(objects[i][0]["day"])
 extension = ".json"
 with open (location+filename+extension, "w") as outfile:
 	json.dump(day, outfile, indent=4)
 
 for i in range(24):
-	location = "./hours/"
+	location = "/home/pi/cyano-automaton.github.io/hours/"
 	filename =str(objects[i][0]["year"])+"_"+str(objects[i][0]["month"])+"_"+str(objects[i][0]["day"])+"_"+str(objects[i][0]["hour"])
 	extension = ".json"
 	remove(location+filename+extension)
