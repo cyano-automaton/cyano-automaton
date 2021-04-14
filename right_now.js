2f1636d99884f1da4bf5722d06e4f275cb796b5b
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2021-04-14 11:48:07 +0300

    Wed 14 Apr 11:48:07 EEST 2021

--- right_now.js
+++ right_now.js
@@ -1,10 +1,13 @@
-var right_now = d3.json("./data/right_now.json", function() {
-  console.log(right_now["temp"]);
+var right_now = d3.json("./data/right_now.json", function(error, data) {
+  if(error == null || error == undefined) {
+    var svgContainer = d3.select("#right_now")
+                      .append("p")
+                      .text(data["temp"])
+} else {
+  window.alert('Something wrong happened while loading the data from JSON file. Try again.');
 });
 
-var svgContainer = d3.select("#right_now")
-                            .append("p")
-                            .text(right_now["temp"])
+
 
 
 /*var right_now = svgContainer.select("#right_now")
