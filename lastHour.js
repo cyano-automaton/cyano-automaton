dcb40ba12528b55477c402cd892eae232489f535
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2021-04-15 00:27:16 +0300

    Thu 15 Apr 00:27:16 EEST 2021

--- lastHour.js
+++ lastHour.js
@@ -1,30 +1,9 @@
-let lastHour;
-let font_size = 20;
+dataset = d3.json("./hours/lastHour.json")
 
-function preload() {
-  let year = year();
-  let month = month();
-  let day = day();
-  let hour = hour() - 2;
-  let adres = "./hours/"+year+"_"+month+"_"+day+"_"+hour;
-  lastHour = loadJSON(adres);
-}
-
-function setup() {
-  canvas = createCanvas(960, font_size*12);
-  canvas.parent('lastHour');
-  textFont("Roboto Mono");
-  noLoop();
-}
-
-function draw() {
-  background(255, 0);
-  textSize(font_size);
-  text("Right now:",0, font_size)
-  text("Temperature: "+right_now.temp+"°C",0, font_size*4)
-  text("TDS: "+right_now.tds+" ppm",0, font_size*6)
-  text("pH: "+right_now.ph,0, font_size*8)
-  text("Turbidity: "+right_now.ntu+" ntu",0, font_size*10)
-  textAlign(RIGHT);
-  text(right_now.hour+":"+right_now.minute+" "+right_now.day+"."+right_now.month+"." +right_now.year,width, font_size)
-}
+svg = d3.select("#last_hour").selectAll("p")
+                        .data(dataset)
+                        .enter()
+                        .appent("p")
+                        .text(function (d) {
+                            return d.minute
+                        })
