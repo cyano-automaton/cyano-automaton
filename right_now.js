0581c9dbb3325749aad610e7c1a5d2ce09a83083
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2021-04-14 11:21:02 +0300

    Wed 14 Apr 11:21:02 EEST 2021

--- right_now.js
+++ right_now.js
@@ -2,7 +2,7 @@ var json = d3.json("./data/right_now.json");
 
 var svgContainer = d3.select("#right_now")
                             .append("p")
-                            .text(json[temp])
+                            .text(json["temp"])
 
 
 /*var right_now = svgContainer.select("#right_now")
