8796b399758621fc95791fba57a665725f2f1f65
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2021-04-14 10:52:52 +0300

    Wed 14 Apr 10:52:52 EEST 2021

--- right_now.js
+++ right_now.js
@@ -1,25 +1,13 @@
-let right_now;
-let font_size = 20;
+const fs = require("fs");
+var json = JSON.parse(fs.readFileSync("./data/right_now.json"));
 
-function preload() {
-  let adres = "./data/right_now.json";
-  right_now = loadJSON(adres);
-}
+var svgContainer = d3.select("body").append("svg")
+                                    .attr("width", 960)
+                                    .attr("height", 540);
 
-function setup() {
-  createCanvas(400, 400);
-  textFont("Roboto Mono");
-  noLoop();
-}
 
-function draw() {
-  background(255, 0);
-  textSize(font_size);
-  text("Right now:",0, font_size)
-  text("Temperature: "+right_now.temp+"°C",0, font_size*4)
-  text("TDS: "+right_now.tds+" ppm",0, font_size*6)
-  text("pH: "+right_now.ph,0, font_size*8)
-  text("Turbidity: "+right_now.ntu+" ntu",0, font_size*10)
-  textAlign(RIGHT);
-  text(right_now.hour+":"+right_now.minute+" "+right_now.day+"."+right_now.month+"." +right_now.year,width, font_size)
-}
+var right_now = svgContainer.selectAll("p")
+                            .data(json)
+                            .enter()
+                            .append("p")
+                            .text("coś")
