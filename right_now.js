5e2a3b8eb45e986b438510e99b54e04455ba2756
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2021-04-14 12:01:09 +0300

    Wed 14 Apr 12:01:09 EEST 2021

--- right_now.js
+++ right_now.js
@@ -1,15 +1,25 @@
-var right_now = d3.json("./data/right_now.json", function(error, data) {
-  if(error == null || error == undefined) {
-    var svgContainer = d3.select("#right_now")
-                      .append("p")
-                      .text(data["temp"])
-} else {
-  window.alert('Something wrong happened while loading the data from JSON file. Try again.');
-}});
+let right_now;
+let font_size = 20;
 
+function preload() {
+  let adres = "./data/right_now.json";
+  right_now = loadJSON(adres);
+}
 
+function setup() {
+  createCanvas(400, 400);
+  textFont("Roboto Mono");
+  noLoop();
+}
 
-
-/*var right_now = svgContainer.select("#right_now")
-                            .append("p")
-                            .text("coś")*/
+function draw() {
+  background(255, 0);
+  textSize(font_size);
+  text("Right now:",0, font_size)
+  text("Temperature: "+right_now.temp+"°C",0, font_size*4)
+  text("TDS: "+right_now.tds+" ppm",0, font_size*6)
+  text("pH: "+right_now.ph,0, font_size*8)
+  text("Turbidity: "+right_now.ntu+" ntu",0, font_size*10)
+  textAlign(RIGHT);
+  text(right_now.hour+":"+right_now.minute+" "+right_now.day+"."+right_now.month+"." +right_now.year,width, font_size)
+}
