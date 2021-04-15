33d6a33d14c2399da4e3f531f600b40278b98009
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2021-04-15 16:22:03 +0300

    Thu 15 Apr 16:22:03 EEST 2021

---
+++ mergeDays (copy).py
@@ -0,0 +1,52 @@
+import json
+from os import listdir, remove
+
+def avg (list):
+	list_sum = 0
+	for i in list:
+		list_sum = list_sum + i
+	avg = list_sum / len(list)
+	return avg
+
+def byHour(e):
+    return e[0]['hour']
+
+def byDay(e):
+    return e[0]['day']
+
+files = listdir("./hours/")
+
+print(files)
+
+objects=[]
+
+for i in files:
+	with open ("./hours/"+i, "r") as f:
+		x=json.loads(f.read())
+		objects.append(x)
+
+objects.sort(key=byHour)
+objects.sort(key=byDay)
+
+day = []
+
+for i in range(24):
+	day.append(objects[i])
+location = "./days/"
+filename =str(objects[i][0]["year"])+"_"+str(objects[i][0]["month"])+"_"+str(objects[i][0]["day"])
+extension = ".json"
+with open (location+filename+extension, "w") as outfile:
+	json.dump(day, outfile, indent=4)
+
+with open (location+"lastDay"+extension, "w") as outfile:
+	json.dump(day, outfile, indent=4)
+
+for i in range(24):
+	location = "./hours/"
+	filename =str(objects[i][0]["year"])+"_"+str(objects[i][0]["month"])+"_"+str(objects[i][0]["day"])+"_"+str(objects[i][0]["hour"])
+	extension = ".json"
+	remove(location+filename+extension)
+	print(location+filename+extension)
+
+for i in range(24):
+	objects.pop(i)
