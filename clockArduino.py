d8f4dcf1714a5405becee6cf1d4d5345eefe4d69
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2021-04-22 16:26:44 +0300

    Thu 22 Apr 16:26:44 EEST 2021

--- clockArduino.py
+++ clockArduino.py
@@ -8,23 +8,19 @@ bubbles = gpiozero.DigitalOutputDevice(21)
 while True:
     now = datetime.now()
 
-    if (now.minute > 45) or (now.hour < 5) or (now.hour > 23):
-        light.off()
-        print("Light turned off")
+    if (now.hour > 5) or (now.hour < 23):
+        if (now.minute < 45):
+            light.on()
+        else:
+            light.off()
     else:
-        light.on()
-        print("Light turned on")
+        light.off()
 
-    print (now.minute%5)
-    print (now.second)
-    if (now.hour > 6) or (now.hour < 22):
+
+    if (now.hour < 6) or (now.hour > 22):
         if (now.minute == 0) and (now.second < 11):
             bubbles.on()
-            print("Bubbles turned on")
     if (now.minute%5 == 0) and (now.second < 11):
         bubbles.on()
-        print("Bubbles turned on")
-
     else:
         bubbles.off()
-        print("Bubbles turned off")
