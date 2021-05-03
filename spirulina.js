9984d0ccc8bc176bd83e56c9b5fa13fdf7741da0
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2021-05-03 17:21:23 +0300

    Mon  3 May 17:21:23 EEST 2021

--- spirulina.js
+++ spirulina.js
@@ -20,3 +20,20 @@ for (i in last24) {
 
 console.log(yesterday);
 console.log(today);
+
+function spirulinaProduced(pomiar) {
+  //wzór do obliczania przejstysci wyrażonej w zanurzeniu dysku secchiego z pomiaru z fotorezystorów
+  secchi = 1150/pomiar^0.7;
+
+  //wzór do obliczania masy spiruliny w gramach w litrze plynu na bazie zanurzenia deysku secchiego
+  spirulina = 70/secchi;
+
+  //ilosc litrow wody w zbiorniku = 5 litres
+  volume = 5;
+
+  return spirulina*volume
+}
+
+console.log("Wczoraj w zbiorniku było: "+spirulinaProduced(yesteday)+" gramów spiruliny.")
+console.log("Dziś jest w zbiorniku: "+spirulinaProduced(today)+" gramów spiruliny.")
+console.log("Czyli wyprodukowanu: "+spirulinaProduced(yesteday)-spirulinaProduced(today)+" gramów spiruliny w ciągu 24h.")
