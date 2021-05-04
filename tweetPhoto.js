750894b76a18edb50c29c4187fef03495bd709e9
Bartlomiej Rey <bartlomiej.rey@gmail.com>
2021-05-04 16:41:44 +0300

    Tue  4 May 16:41:44 EEST 2021

--- tweetPhoto.js
+++ tweetPhoto.js
@@ -5,6 +5,10 @@ let year_json = fs.readFileSync('./yearForToday.json');
 let year_object = JSON.parse(year_json);
 let year = year_object.year;
 
+let captions_json = fs.readFileSync('./nasa/captions.json');
+let caption = JSON.parse(captions_json);
+let kolejnosc = year-1959;
+
 
 const Twit = require("twit"),
   config = {
@@ -26,21 +30,31 @@ let listener = app.listen(process.env.PORT, function() {
 
 var b64content = fs.readFileSync('./nasa/'+year+".jpg", { encoding: 'base64' })
 
+console.log("Length of caption = " + caption[kolejnosc].length)
+
+if (caption[kolejnosc].length > 296) {
+  text = caption[kolejnosc].slice(0,296);
+  text = text + "...";
+} else {
+  var text = caption[kolejnosc];
+}
+
 // first we must post the media to Twitter
 T.post('media/upload', { media_data: b64content }, function (err, data, response) {
   // now we can assign alt text to the media, for use by screen readers and
   // other text-based presentations and interpreters
   var mediaIdStr = data.media_id_string
-  var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+  var altText = text
   var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
 
   T.post('media/metadata/create', meta_params, function (err, data, response) {
     if (!err) {
       // now we can reference the media and post a tweet (media will attach to the tweet)
-      var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
+      var params = { status: text, media_ids: [mediaIdStr] }
 
       T.post('statuses/update', params, function (err, data, response) {
         console.log(data)
+        process.exit()
       })
     }
   })
--- yearForToday.py
+++ yearForToday.py
@@ -4,7 +4,7 @@ import json
 with open ("./yearForToday.json", "r") as file:
 	year = json.loads(file.read())
 
-year.year = year.year + 1
+year["year"] = year["year"] + 1
 
 with open ("./yearForToday.json", "w") as file:
 	json.dump(year, file,  indent=4)
